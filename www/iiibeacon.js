fb02258c229c2a849f20a50e890b1fc74b32a2b0
guaiguaiob <guaiguaiob@outlook.com>
2016-11-16 12:12:21 +0800

    Update iiibeacon.js

--- www/iiibeacon.js
+++ www/iiibeacon.js
@@ -1,1 +1,15 @@
+/*global cordova, module*/
+var exec = cordova.require('cordova/exec');
+var iiibeacon = iiibeacon || {};
+module.exports = iiibeacon;
+
+iiibeacon.greet = function (name, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "iiibeacon", "greet", [name]);
+    return true;
+};
+
+iiibeacon.init = function (successCallback, errorCallback) {
+	exec(successCallback, errorCallback, "iiibeacon", "init", []);
+	return true;
+};
 
